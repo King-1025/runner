54b4fc430ece9f004200c55dff801527eba8b007
King-1025 <1543641386@qq.com>
2019-12-18 16:50:01 +0800

    fix

--- ffmpeg-build.sh
+++ ffmpeg-build.sh
@@ -2,6 +2,7 @@
 
 ROOT=$(pwd)
 FFMPEG_DIR=$ROOT/ffmpeg
+X264_DIR=$ROOT/x264
 INSTALL_DIR=$ROOT/result/ffmpeg
 YASM="yasm-1.3.0"
 
@@ -9,13 +10,32 @@ YASM="yasm-1.3.0"
 mkdir -p $FFMPEG_DIR
 mkdir -p $INSTALL_DIR
 
-wget http://www.tortall.net/projects/yasm/releases/$YASM.tar.gz && tar -xzvf $YASM.tar.gz && cd $YASM && ./configure && make -j 4 && sudo make install \
+wget http://www.tortall.net/projects/yasm/releases/$YASM.tar.gz \
+&& \
+tar -xzf $YASM.tar.gz \
+&& \
+cd $YASM \
+&& ./configure \
+&& \
+make -j 4 \
+&& \
+sudo make install \
+&& \
+git clone https://github.com/mirror/x264.git $X264_DIR -j 4 \
+&& \
+cd $X264_DIR \
+&& \
+./configure --prefix=/opt/x264_install --enable-static --disable-shared \
+&& \
+make -j 4 \
+&& \
+make install \
 && \
 git clone https://git.ffmpeg.org/ffmpeg.git $FFMPEG_DIR -j 4 \
 && \
 cd $FFMPEG_DIR \
 && \
-./configure --prefix=$INSTALL_DIR \
+./configure --prefix=$INSTALL_DIR --enable-gpl --enable-version3 --enable-static --enable-libx264 --extra-cflags='-I/opt/x264_install/include' --extra-ldflags='-L/opt/x264_install/lib -lx264 -lpthread -lm -ldl' --enable-static --disable-shared --enable-pthreads --enable-encoder=libx264 \
 && \
 make -j 4 \
 && \
