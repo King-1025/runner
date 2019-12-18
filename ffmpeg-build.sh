30d05422863d8b0a40872412d4ec04afdac7f037
King-1025 <1543641386@qq.com>
2019-12-18 16:21:45 +0800

    fix

--- ffmpeg-build.sh
+++ ffmpeg-build.sh
@@ -9,7 +9,7 @@ YASM="yasm-1.3.0"
 mkdir -p $FFMPEG_DIR
 mkdir -p $INSTALL_DIR
 
-wget http://www.tortall.net/projects/yasm/releases/$YASM.tar.gz && tar -xzvf $YASM.tar.gz && cd $YASM && ./configure && make -j 4 && make install \
+wget http://www.tortall.net/projects/yasm/releases/$YASM.tar.gz && tar -xzvf $YASM.tar.gz && cd $YASM && ./configure && make -j 4 && sudo make install \
 && \
 git clone https://git.ffmpeg.org/ffmpeg.git $FFMPEG_DIR -j 4 \
 && \
