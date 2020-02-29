320462d89bbaf99854402df7eb3f5c0926abb7c6
King-1025 <1543641386@qq.com>
2020-02-29 18:24:57 +0800

    add gf.sh

--- explore.sh
+++ explore.sh
@@ -21,4 +21,7 @@ rm -rf $RESULT && mkdir -p $RESULT
 #check "whoami"
 #check "$ROOT/ffmpeg-build.sh"
 #check "ping -c 3 google.com"
-check "$ROOT/xy998.sh"
+#check "$ROOT/xy998.sh"
+check "$ROOT/gf.sh json"
+check "echo && echo && echo"
+check "$ROOT/gf.sh"
