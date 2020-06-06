81cbf5058da2f35cb22448ac3f440550f2f8b2a4
King-1025 <1543641386@qq.com>
2020-06-06 18:56:11 +0800

    增加安卓构建支持 fix

--- tool/bin/android_build.sh
+++ tool/bin/android_build.sh
@@ -27,11 +27,17 @@ rm -rf $OUTPUT && mkdir $OUTPUT
 for i in $(find $RELEASE -name *.apk -print); do
    in=$i
    out="$OUTPUT/$(echo $(basename $i) | sed "s/un//g")"
+   echo sign $in...
    apksigner sign -v --ks $KS --in $in --out $out --ks-pass file:$KSP
+   if [ $? -eq 0 ]; then
+      echo "save as $out"
+   fi
+   echo ""
 done
 
 if [ ! -z $ESC_KEY ]; then
-   esc "$(echo $ESC_KEY | awk -F "/" '{print $1" "$2}')" "$OUTPUT"
+   comm="esc $(echo $ESC_KEY | awk -F "/" '{print $1" "$2}') $OUTPUT"
+   $comm
 else
    echo "not found! ESC_KEY"
    exit 1
