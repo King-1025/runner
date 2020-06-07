965b6970e00214c11a99323596d25583ba43676c
King-1025 <1543641386@qq.com>
2020-06-07 12:27:49 +0800

    增加安卓构建支持 fix

--- script/android-tv.sh
+++ script/android-tv.sh
@@ -2,4 +2,12 @@
 
 git clone https://github.com/android/tv-samples.git -j 4
 
-android_build ./tv-samples/LeanbackShowcase
+subdir=./tv-samples/Leanback
+
+path=$(find $subdir -name "Android*" -print)
+
+sed "s/LAUNCHER/#/g" $path | sed "s/LEANBACK_#/LAUNCHER/g" | sed "s/#/LEANBACK_LAUNCHER/g" > $path
+
+#android_build ./tv-samples/LeanbackShowcase
+
+android_build $subdir
