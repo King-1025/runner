adb493f9a7b3153272811ed3d6d78351e3982e69
King-1025 <1543641386@qq.com>
2020-06-07 14:39:33 +0800

    增加安卓构建支持 fix

--- script/android-tv.sh
+++ script/android-tv.sh
@@ -6,11 +6,16 @@ root=$(pwd)
 
 subdir=$root/tv-samples/Leanback
 
+buildf="app/build.gradle"
+
 path=$(find $subdir -name "Android*" -print)
 
 sed -i "s/LAUNCHER/#/g"          $path 
 sed -i "s/LEANBACK_#/LAUNCHER/g" $path 
 sed -i "s/#/LEANBACK_LAUNCHER/g" $path
+
+sed -i "s/21/17/g" "$subdir/$buildf"
+sed -i "s/28/21/g" "$subdir/$buildf"
 
 android_build $subdir
 
@@ -19,5 +24,8 @@ subdir=$root/tv-samples/LeanbackShowcase
 path=$(find $subdir -name "Android*" -print)
 
 sed -i "s/LEANBACK_//g" $path
+
+sed -i "s/21/17/g" "$subdir/$buildf"
+sed -i "s/28/21/g" "$subdir/$buildf"
 
 android_build $subdir
