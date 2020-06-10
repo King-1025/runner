0478ede56bc74ad2416fa92e9a14a889888ae231
King-1025 <1543641386@qq.com>
2020-06-10 18:58:46 +0800

    添加win分支，执行win环境勘探 fix

--- script/fitx修复安装.cmd
+++ script/fitx修复安装.cmd
@@ -14,7 +14,9 @@ pip install virtualenvwrapper-win
 echo
 
 echo 你可以设置环境变量: WORKON_HOME 作为python虚拟环境的存放目录。
+setx /m WORKON_HOME %RESULT%
 rem pause
+echo RESULT: %RESULT%
 
 echo 使用mkvirtualenv，创建fitx虚拟环境
 mkvirtualenv fitx
